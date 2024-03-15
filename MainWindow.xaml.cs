d7b8cce10ae4db1c159bfa8f54828928749b1868
Minerva_juppiter <ryouturn@gmail.com>
2024-03-15 20:03:20 +0900

    スライダーの隣に数字のテキストボックスを追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,53 @@ namespace BusynessNotification
         {
             InitializeComponent();
         }
+
+        private void CPUSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int CPUvalue = (int)e.NewValue;
+            TextBox_CPUSlider.Text = CPUvalue.ToString();
+            CPUSlider.Value = CPUvalue;
+        }
+
+        private void TextBox_CPUSlider_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextBox_CPUSlider.ToString() != "")
+            {
+                double CPUvalue = double.Parse(TextBox_CPUSlider.Text);
+                CPUSlider.Value = CPUvalue;
+            }
+        }
+
+        private void TextBox_MemorySlider_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextBox_CPUSlider.ToString() != "")
+            {
+                double Memoryvalue = double.Parse(TextBox_MemorySlider.Text);
+                CPUSlider.Value = Memoryvalue;
+            }
+        }
+
+        private void MemorySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int Memoryvalue = (int)e.NewValue;
+            TextBox_CPUSlider.Text = Memoryvalue.ToString();
+            CPUSlider.Value = Memoryvalue;
+        }
+
+        private void DiskSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int Diskvalue = (int)e.NewValue;
+            TextBox_DiskSlider.Text = Diskvalue.ToString();
+            DiskSlider.Value = Diskvalue;
+        }
+
+        private void TextBox_DiskSlider_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextBox_DiskSlider.Text.ToString() != "")
+            {
+                double Diskvalue = double.Parse (TextBox_DiskSlider.Text);
+                DiskSlider.Value = Diskvalue;
+            }
+        }
     }
 }
